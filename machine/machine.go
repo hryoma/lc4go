2058a4cbaa4a70e3a0181b838c479465b24664a0
hryoma <84463842+hryoma@users.noreply.github.com>
2022-12-12 11:07:58 -0500

    sign extension and test

--- machine/machine.go
+++ machine/machine.go
@@ -49,9 +49,20 @@ const (
 	OpLC
 )
 
+const (
+	nzpNONE	= 0b00000000
+	nzpN	= 0b00000100
+	nzpZ	= 0b00000010
+	nzpP	= 0b00000001
+	nzpNZ	= 0b00000110
+	nzpNP	= 0b00000101
+	nzpZP	= 0b00000011
+	nzpNZP	= 0b00000111
+)
+
 type Insn struct {
 	Data		uint16
-	Op			int
+	Op			Op
 	String		string
 	Breakpoint	bool
 }
@@ -64,5 +75,86 @@ type Machine struct {
 	Psr		uint16
 	Pc		uint16
 	Labels	map[string]uint16
+}
+
+func getSignExtN(data uint16, nBits uint16) (signExtData uint16) {
+	// get the sign and generate a mask
+	var sign uint16 = data & (1 << (nBits - 1))
+	var mask uint16 = (0xFF << nBits)
+
+	// sign extend it
+	if sign == 0 {
+		signExtData = data & ^mask
+	} else {
+		signExtData = data | mask
+	}
+
+	return
+}
+
+func Execute(lc4 *Machine) (err int) {
+	// check if PC is at the end
+	if lc4.Pc == 0x80FF {
+		return 0
+	} else if lc4.Pc < 0x0000 {
+		// TODO - update these with the correct values
+		// TODO - check for data/code region, privilege bit, etc.
+		// PC is out of bounds
+		return -1
+	} else if lc4.Pc > 0xFFFF {
+		// PC is out of bounds
+		return -1
+	}
+
+	insn := lc4.Code[lc4.Pc]
+	switch insn.Op {
+		case OpNOP:
+			lc4.Pc += 1
+		case OpBRp:
+			lc4.Pc += 1
+			if lc4.Nzp == nzpP {
+				lc4.Pc += getSignExtN(insn.Data, 9)
+			}
+		case OpBRz:
+		case OpBRzp:
+		case OpBRn:
+		case OpBRnp:
+		case OpBRnz:
+		case OpBRnzp:
+		case OpADD:
+		case OpMUL:
+		case OpSUB:
+		case OpDIV:
+		case OpADDI:
+		case OpCMP:
+		case OpCMPU:
+		case OpCMPI:
+		case OpCMPIU:
+		case OpJSRR:
+		case OpJSR:
+		case OpAND:
+		case OpNOT:
+		case OpOR:
+		case OpXOR:
+		case OpANDI:
+		case OpLDR:
+		case OpSTR:
+		case OpRTI:
+		case OpCONST:
+		case OpSLL:
+		case OpSRA:
+		case OpSRL:
+		case OpMOD:
+		case OpJMPR:
+		case OpJMP:
+		case OpHICONST:
+		case OpTRAP:
+		// psuedo instructions:
+		case OpRET:
+		case OpLEA:
+		case OpLC:
+	}
+
+	return 0
 }
 
---
+++ machine/machine_test.go
@@ -0,0 +1,27 @@
+package machine
+
+import (
+	"testing"
+)
+
+func TestGetSignExtNPos(t *testing.T) {
+	var nBits uint16 = 9
+	//                    |   |   |   |   |
+	var testData uint16 = 0b1010101010101010
+	var expected uint16 = 0b0000000010101010
+	var actual uint16 = getSignExtN(testData, nBits)
+	if actual != expected {
+		t.Error("Sign extension failed for positive int. Expected", expected, "but got", actual)
+	}
+}
+
+func TestGetSignExtNNeg(t *testing.T) {
+	var nBits uint16 = 9
+	//                    |   |   |   |   |
+	var testData uint16 = 0b0101010101010101
+	var expected uint16 = 0b1111111101010101
+	var actual uint16 = getSignExtN(testData, nBits)
+	if actual != expected {
+		t.Error("Sign extension failed for negative int. Expected", expected, "but got", actual)
+	}
+}

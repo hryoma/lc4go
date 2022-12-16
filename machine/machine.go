bf91f596a15798578e61ed55c30e6508b4a15380
hryoma <84463842+hryoma@users.noreply.github.com>
2022-12-16 11:30:48 -0500

    refactor machine execute func layout, added semantics

--- machine/machine.go
+++ machine/machine.go
@@ -99,68 +99,107 @@ func Execute() (err int) {
 
 	insn := Lc4.Code[Lc4.Pc]
 	switch insn.Op {
+		// branch instructions
 		case OpNOP:
+			// PC = PC + 1
 			Lc4.Pc += 1
 		case OpBRp:
-			Lc4.Pc += 1
+			// if P, PC = PC + 1 + sext(IMM9)
 			if Lc4.Nzp > 0 {
+				Lc4.Pc += 1
 				Lc4.Pc = uint16(int32(Lc4.Pc) + int32(getSignExtN(insn.Data, 9)))
 			}
 		case OpBRz:
-			Lc4.Pc += 1
+			// if Z, PC = PC + 1 + sext(IMM9)
 			if Lc4.Nzp == 0 {
+				Lc4.Pc += 1
 				Lc4.Pc = uint16(int32(Lc4.Pc) + int32(getSignExtN(insn.Data, 9)))
 			}
 		case OpBRzp:
-			Lc4.Pc += 1
+			// if Z/P, PC = PC + 1 + sext(IMM9)
 			if Lc4.Nzp >= 0 {
+				Lc4.Pc += 1
 				Lc4.Pc = uint16(int32(Lc4.Pc) + int32(getSignExtN(insn.Data, 9)))
 			}
 		case OpBRn:
-			Lc4.Pc += 1
+			// if N, PC = PC + 1 + sext(IMM9)
 			if Lc4.Nzp < 0 {
+				Lc4.Pc += 1
 				Lc4.Pc = uint16(int32(Lc4.Pc) + int32(getSignExtN(insn.Data, 9)))
 			}
 		case OpBRnp:
-			Lc4.Pc += 1
+			// if NP, PC = PC + 1 + sext(IMM9)
 			if Lc4.Nzp != 0 {
+				Lc4.Pc += 1
 				Lc4.Pc = uint16(int32(Lc4.Pc) + int32(getSignExtN(insn.Data, 9)))
 			}
 		case OpBRnz:
-			Lc4.Pc += 1
+			// if NZ, PC = PC + 1 + sext(IMM9)
 			if Lc4.Nzp <= 0 {
+				Lc4.Pc += 1
 				Lc4.Pc = uint16(int32(Lc4.Pc) + int32(getSignExtN(insn.Data, 9)))
 			}
 		case OpBRnzp:
+			// if NZP, PC = PC + 1 + sext(IMM9)
 			Lc4.Pc += 1
 			Lc4.Pc = uint16(int32(Lc4.Pc) + int32(getSignExtN(insn.Data, 9)))
+		// arithmetic instructions
 		case OpADD:
+			// Rd = Rs + Rt
 			rd := insn.Data & (0b0111 << 9)
 			rs := insn.Data & (0b0111 << 6)
 			rt := insn.Data & (0b0111)
 			Lc4.Reg[rd] = Lc4.Reg[rs] + Lc4.Reg[rt]
 		case OpMUL:
+			// Rd = Rs * Rt
 			rd := insn.Data & (0b0111 << 9)
 			rs := insn.Data & (0b0111 << 6)
 			rt := insn.Data & (0b0111)
 			Lc4.Reg[rd] = Lc4.Reg[rs] * Lc4.Reg[rt]
 		case OpSUB:
+			// Rd = Rs - Rt
 			rd := insn.Data & (0b0111 << 9)
 			rs := insn.Data & (0b0111 << 6)
 			rt := insn.Data & (0b0111)
 			Lc4.Reg[rd] = Lc4.Reg[rs] - Lc4.Reg[rt]
 		case OpDIV:
+			// Rd = Rs / Rt
 			rd := insn.Data & (0b0111 << 9)
 			rs := insn.Data & (0b0111 << 6)
 			rt := insn.Data & (0b0111)
 			Lc4.Reg[rd] = Lc4.Reg[rs] / Lc4.Reg[rt]
 			// TODO error handling for div by 0
 		case OpADDI:
+			// Rd = Rs + sext(IMM5)
 			rd := insn.Data & (0b0111 << 9)
 			rs := insn.Data & (0b0111 << 6)
 			imm := getSignExtN(insn.Data, 5)
 			Lc4.Reg[rd] = int16(int32(Lc4.Reg[rs]) + int32(imm))
+		case OpMOD:
+			// Rd = Rs % Rt
+		// logical instructions
+		case OpAND:
+			// Rd = Rs & Rt
+		case OpNOT:
+			// Rd = ~Rs
+		case OpOR:
+			// Rd = Rs | Rt
+		case OpXOR:
+			// Rd = Rs ^ Rt
+		case OpANDI:
+			// Rd = Rs & sext(IMM5)
+		// mem instructions
+		case OpLDR:
+			// Rd = dmem[Rs + sext(IMM6)]
+		case OpSTR:
+			// dmem[Rs + sext(IMM6)] = Rt
+		case OpCONST:
+			// Rd = sext(IMM9)
+		case OpHICONST:
+			// Rd = (Rd & 0xFF) | (UIMM8 << 8)
+		// comparison instructions
 		case OpCMP:
+			// NZP = sign(Rs - Rt)
 			rs := insn.Data & (0b0111 << 9)
 			rt := insn.Data & (0b0111)
 			diff := Lc4.Reg[rs] - Lc4.Reg[rt]
@@ -172,6 +211,7 @@ func Execute() (err int) {
 				Lc4.Nzp = 1
 			}
 		case OpCMPU:
+			// NZP = sign(uRs - uRt)
 			rs := insn.Data & (0b0111 << 9)
 			rt := insn.Data & (0b0111)
 			diff := int16(uint16(Lc4.Reg[rs]) - uint16(Lc4.Reg[rt]))
@@ -183,6 +223,7 @@ func Execute() (err int) {
 				Lc4.Nzp = 1
 			}
 		case OpCMPI:
+			// NZP = sign(Rs - IMM7)
 			rs := insn.Data & (0b0111 << 9)
 			imm := getSignExtN(insn.Data, 7)
 			diff := Lc4.Reg[rs] - int16(imm)
@@ -194,6 +235,7 @@ func Execute() (err int) {
 				Lc4.Nzp = 1
 			}
 		case OpCMPIU:
+			// NZP = sign(uRs - UIMM7)
 			rs := insn.Data & (0b0111 << 9)
 			imm := getSignExtN(insn.Data, 7)
 			diff := int16(uint16(Lc4.Reg[rs]) - imm)
@@ -204,34 +246,39 @@ func Execute() (err int) {
 			} else {
 				Lc4.Nzp = 1
 			}
+		// shift instructions
+		case OpSLL:
+			// Rd = Rs << UIMM4
+		case OpSRA:
+			// Rd = Rs >>> UIMM4
+		case OpSRL:
+			// Rd = Rs >> UIMM4
+		// jjump instructions
 		case OpJSRR:
+			// R7 = PC + 1; PC = Rs
 			rsVal := Lc4.Reg[insn.Data & (0b0111 << 6)]
 			Lc4.Reg[7] = int16(Lc4.Pc) + 1
 			Lc4.Pc = uint16(rsVal)
 		case OpJSR:
+			// R7 = PC + 1; PC = (PC & 0x8000) | (IMM11 << 4)
 			Lc4.Reg[7] = int16(Lc4.Pc) + 1
 			Lc4.Pc = (Lc4.Pc & 0x8000) | uint16(getSignExtN(insn.Data, 11) << 4)
-		case OpAND:
-		case OpNOT:
-		case OpOR:
-		case OpXOR:
-		case OpANDI:
-		case OpLDR:
-		case OpSTR:
-		case OpRTI:
-		case OpCONST:
-		case OpSLL:
-		case OpSRA:
-		case OpSRL:
-		case OpMOD:
 		case OpJMPR:
+			// PC = Rs
 		case OpJMP:
-		case OpHICONST:
+			// PC = PC + 1 + sext(IMM11)
+		// privilege instructions
 		case OpTRAP:
+			// R7 = PC + 1; PC = (0x8000 | IMM8); PSR[15] = 1
+		case OpRTI:
+			// PC = R7; PSR[15] = 0
 		// psuedo instructions:
 		case OpRET:
+			// return to R7
 		case OpLEA:
+			// store address of <Label> in Rd
 		case OpLC:
+			// store value of constant <Label> in Rd
 	}
 
 	return 0
